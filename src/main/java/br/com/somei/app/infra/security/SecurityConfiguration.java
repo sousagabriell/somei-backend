b2394be816cb8cec35be08162c07b8ab04c8ba43
Eduardo Alves <eduardo.alvesrod@gmail.com>
2023-07-19 19:59:16 -0300

    fix: corerecao login

--- src/main/java/br/com/somei/app/infra/security/SecurityConfiguration.java
+++ src/main/java/br/com/somei/app/infra/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration  {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers(HttpMethod.POST, "/login/*").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/autenticacao/*").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
